d9b5f21343083f02d9905dde7b4871afca3e5563
why <1621552691@qq.com>
2023-01-04 21:24:46 +0800

    23-1-4

--- LeetCode/src/test/java/com/test/test01.java
+++ LeetCode/src/test/java/com/test/test01.java
@@ -3,39 +3,41 @@
 //import com.why.leetcode.pojo.ListNode;
 //import org.junit.Test;
 //
+//import java.io.File;
+//import java.io.IOException;
+//import java.nio.file.Files;
+//import java.nio.file.Path;
+//import java.nio.file.Paths;
 //import java.util.*;
 //import java.util.function.BiConsumer;
 //import java.util.function.BiFunction;
+//import java.util.function.Consumer;
 //
 //public class test01 {
 //    @Test
 //    public void test() {
-//        int[] nums1 = {1,1,3,2} , nums2 = {2,3}, nums3 = {3};
-//        twoOutOfThree(nums1 , nums2 , nums3);
+//        String s = "4 5 11 26";
+//        System.err.println(areNumbersAscending(s));
 //    }
 //
-//    public List<Integer> twoOutOfThree(int[] nums1, int[] nums2, int[] nums3) {
-//        List<Integer> result = new ArrayList<>();
-//        Map<Integer , Integer> map1 = new HashMap<>();
-//        for(int n : nums1)
-//            map1.put(n , 1);
-//
-//        Map<Integer , Integer> map2 = new HashMap<>();
-//        for(int n : nums2)
-//            map2.put(n , 1);
-//
-//        Map<Integer , Integer> map3 = new HashMap<>();
-//        for(int n : nums3)
-//            map3.put(n , 1);
-//
-//        map2.forEach((k, v) -> map1.merge(k, v, Integer::sum));
-//        map3.forEach((k, v) -> map1.merge(k, v, Integer::sum));
-//
-//        for (Map.Entry<Integer, Integer> entry : map1.entrySet()) {
-//            if(entry.getValue() >= 2)
-//                result.add(entry.getKey());
+//    public boolean areNumbersAscending(String s) {
+//        int n = 0 , j = -1;
+//        for(int i = 0 ; i < s.length() - 1 ; i++){
+//            char c = s.charAt(i);
+//            if(Character.isDigit(c)){
+//                n += c - '0';
+//                if(Character.isDigit(s.charAt(i + 1)))
+//                    n *= 10;
+//                else{
+//                    if(j < n){
+//                        j = n;
+//                        n = 0;
+//                    }
+//                }
+//            }else
+//                continue;
 //        }
-//        return result;
+//        return j == n;
 //    }
 //
 //}
